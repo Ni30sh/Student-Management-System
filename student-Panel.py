c46cca67c16039138f879d1b0fd86f8163c02726
Ni30shkumar <addy2166@gmail.com>
2023-07-29 21:46:15 +0530

    code

--- student-Panel.py
+++ student-Panel.py
@@ -5,7 +5,9 @@ import time
 from tkinter.ttk import Treeview
 from tkinter import ttk
 import mysql.connector
-##################################################$$$$$$$$$$$$ ADDMISSION $$$$$$$$$$$##################################
+
+
+# #################################################ADDMISSION ##################################
 def student_admission():
     def submit_details():
         global conn, cur
@@ -21,14 +23,15 @@ def student_admission():
         strn = "USE studentpanel"
         cur.execute(strn)
         strn = "SELECT * FROM INFORMATION  WHERE ID = %s"
-        cur.execute(strn,(account_val,))
+        cur.execute(strn, (account_val,))
         existing_data = cur.fetchall()
         if existing_data:
             messagebox.showinfo("Notification", "ID Already Exist")
         else:
             cur = conn.cursor()
             cur.execute("INSERT INTO INFORMATION VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                    (account_val, name_val, aadhar_val, dob_val, address_val, contact_val, email_val, gender_val, date))
+                        (account_val, name_val, aadhar_val, dob_val, address_val, contact_val, email_val, gender_val,
+                         date))
             conn.commit()
             strin = "SELECT * FROM INFORMATION"
             cur.execute(strin)
@@ -36,8 +39,8 @@ def student_admission():
             student_details.delete(*student_details.get_children())
             for i in xt:
                 student_details.insert("", END, values=i)  # [i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8]])
-            box = messagebox.askyesnocancel("Notification", "ADDED SUCCESSFULLY... WANT TO CLEAR THE FORM")
-            if box == True:
+            message_box = messagebox.askyesnocancel("Notification", "ADDED SUCCESSFULLY... WANT TO CLEAR THE FORM")
+            if message_box == True:
                 ID.set("")
                 NAME.set("")
                 AADHAR.set("")
@@ -46,13 +49,13 @@ def student_admission():
                 MOBILE.set("")
                 EMAIL.set("")
                 GENDER.set("")
+
     admission_window = Toplevel()
     admission_window.grab_set()
     admission_window.geometry("420x410+800+130")  # W X H
     admission_window.title("Admission Shell")
     admission_window.resizable(False, False)
     admission_window.config(bg="white")
-    #####################################____________________________________
     suggest_label = Label(admission_window, text="Fill the Details of Student", bg="white",
                           font=("Time in romana", 14, "bold"))
     suggest_label.place(x=20, y=10)
@@ -120,9 +123,11 @@ def student_admission():
                         activebackground="white", bg="blue", command=submit_details, fg="white")
     submit_btn.place(x=120, y=350, width=200)
     admission_window.mainloop()
-############################################ LABEL OF FEE SUBMITTING ####################################################
+
+
+# ########################################### LABEL OF FEE SUBMITTING #################################################
 def fee_submit():
-    def Submit_fee():
+    def submit_fee():
         global conn, cur
         aadhar_number = entry_aadhar.get()
         fee_amount = entry_fee.get()
@@ -135,24 +140,25 @@ def fee_submit():
         cur.execute("SELECT * FROM INFORMATION WHERE aadhar = %s", (aadhar_number,))
         result = cur.fetchone()
         if existing_data:
-            messagebox.showinfo("Notification", "Fee Already Submit",parent=fee_window)
+            messagebox.showinfo("Notification", "Fee Already Submit", parent=fee_window)
         elif result:
             # Aadhar number exists, submit to the 'SUBMIT' table
             cur.execute("INSERT INTO SUBMIT (aadhar, fee) VALUES (%s, %s)", (aadhar_number, fee_amount))
             conn.commit()
-            messagebox.showinfo("Notification", "Successfully submitted!",parent=fee_window)
+            messagebox.showinfo("Notification", "Successfully submitted!", parent=fee_window)
         else:
             # Aadhar number does not exist
-            messagebox.showerror("ERROR", "No Student of this Aadhar.",parent=fee_window)
+            messagebox.showerror("ERROR", "No Student of this Aadhar.", parent=fee_window)
             # cur.close()
+
     fee_window = Toplevel()
     fee_window.grab_set()
     fee_window.geometry('300x300+700+199')
     fee_window.title("Fee Window")
-    ############################################################# Entry and Command ######################################
+    # ########################################################## Entry and Command ####################################
     entry_aadhar = StringVar()
     entry_fee = StringVar()
-    suggest_label = Label(fee_window, text="Enter Aadhar and Amount",font=("Microsoft YaHei UI Light", 12, "bold"))
+    suggest_label = Label(fee_window, text="Enter Aadhar and Amount", font=("Microsoft YaHei UI Light", 12, "bold"))
     suggest_label.place(x=5, y=10, height=25, width=220)
     aadhar_label = Label(fee_window, text="Aadhar :", font=("Microsoft YaHei UI Light", 11, "bold"))
     aadhar_label.place(x=10, y=60, height=25, width=90)
@@ -164,28 +170,32 @@ def fee_submit():
     amount_entry = Entry(fee_window, textvariable=entry_fee, bd=0, font=("Microsoft YaHei UI Light", 11, "bold"))
     amount_entry.place(x=20, y=145, height=25, width=260)
     Frame(fee_window, width=260, height=2, bg="black").place(x=20, y=168)
-    submit_fee_button = Button(fee_window, text="Submit", bg="blue", fg="white",font=("chiller", 11, "bold")
-                               ,activebackground="white",activeforeground="black",command=Submit_fee)
-    submit_fee_button.place(x=100, y=210,height=35, width=100)
+    submit_fee_button = Button(fee_window, text="Submit", bg="blue", fg="white", font=("chiller", 11, "bold"),
+                               activebackground="white", activeforeground="black", command=submit_fee)
+    submit_fee_button.place(x=100, y=210, height=35, width=100)
     fee_window.mainloop()
+
+
 ##########################################################  search_student    ##########################################
 def search_student():
     aadhar_no = StringVar()
+
     def show_student_details():
         global conn, cur
         aadhar_number = aadhar_no.get()
         strn = "USE studentpanel"
         cur.execute(strn)
-        # check if Aadhar is exists in Database
+        # check if Aadhar is existing in Database
         strn = "SELECT * FROM INFORMATION WHERE AADHAR = %s"
         cur.execute(strn, (aadhar_number,))
         results = cur.fetchall()
         if results:
-           student_details.delete(*student_details.get_children())
-           for result in results:
-              student_details.insert("", END, values=result)
+            student_details.delete(*student_details.get_children())
+            for result in results:
+                student_details.insert("", END, values=result)
         else:
-            messagebox.showinfo("Notification", "No Aadhar Number Exist.",parent=search_window)
+            messagebox.showinfo("Notification", "No Aadhar Number Exist.", parent=search_window)
+
     search_window = Toplevel()
     search_window.grab_set()
     search_window.geometry('400x300+700+130')
@@ -202,7 +212,9 @@ def search_student():
                                  command=show_student_details, bg="blue", fg="white")
     show_details_button.place(x=135, y=180, width=150, height=35)
     search_window.mainloop()
-########################################################### PRINCIPAL ACCESS ##########################################
+
+
+# ########################################################## PRINCIPAL ACCESS ##########################################
 def principal_access():
     def login():
         entered_id = id_entry.get()
@@ -211,8 +223,10 @@ def principal_access():
             open_principal_portal()
         else:
             messagebox.showerror("Error", "Invalid ID or Password")
+
     def open_principal_portal():
         aadhar_no = StringVar()
+
         def show_aadhar_details():
             global conn, cur
             aadhar_number = aadhar_no.get()
@@ -222,11 +236,12 @@ def principal_access():
             cur.execute(strn, (aadhar_number,))
             results = cur.fetchall()
             if results:
-                 student_details.delete(*student_details.get_children())
-                 for result in results:
-                       student_details.insert("", END, values=result)
+                student_details.delete(*student_details.get_children())
+                for result in results:
+                    student_details.insert("", END, values=result)
             else:
-                messagebox.showinfo("Notification", "No Admission",parent=principal_window)
+                messagebox.showinfo("Notification", "No Admission", parent=principal_window)
+
         def show_fee_details():
             global conn, cur
             aadhar_number1 = aadhar_no.get()
@@ -239,27 +254,32 @@ def principal_access():
                 fee_details = results[0]
                 # for result in results:
                 #     fee_details += f"Aadhar : {result[0]}\nFee: {result[1]}\n\n"
-                messagebox.showinfo("Fee Details", f"Aadhar: {aadhar_number1}\n\nFee : {fee_details[1]}",parent=principal_window)
+                messagebox.showinfo("Fee Details", f"Aadhar: {aadhar_number1}\n\nFee : {fee_details[1]}",
+                                    parent=principal_window)
             else:
-                messagebox.showinfo("Fee Details ", "No Fee Details found for the given Aadhar number",parent=principal_window)
+                messagebox.showinfo("Fee Details ", "No Fee Details found for the given Aadhar number",
+                                    parent=principal_window)
+
         principal_window = Toplevel()
         principal_window.geometry('350x300+700+130')
         principal_window.title("Principal Portal")
-        suggest_label = Label(principal_window, text="Aadhar No. To Get Student Profile",font=('chiller', 11, 'bold'))
-        suggest_label.place(x=6, y=20, height=25, width=310)
-        aadhar_label = Label(principal_window, text="Enter Aadhar Number:",font=('chiller', 10, 'bold'))
+        suggest_label1 = Label(principal_window, text="Aadhar No. To Get Student Profile", font=('chiller', 11, 'bold'))
+        suggest_label1.place(x=6, y=20, height=25, width=310)
+        aadhar_label = Label(principal_window, text="Enter Aadhar Number:", font=('chiller', 10, 'bold'))
         aadhar_label.place(x=12, y=77, height=25, width=180)
-        aadhar_entry = Entry(principal_window, textvariable=aadhar_no, bd=0,font=('chiller', 13, 'bold'))
+        aadhar_entry = Entry(principal_window, textvariable=aadhar_no, bd=0, font=('chiller', 13, 'bold'))
         aadhar_entry.place(x=20, y=110, height=25, width=260)
         Frame(principal_window, width=260, height=2, bg="black").place(x=20, y=135)
-
-        show_details_button = Button(principal_window, text="Details",activeforeground="black",activebackground="white",
-                                     command=show_aadhar_details,font=('chiller', 10, 'bold'),fg="white",bg="blue")
+        show_details_button = Button(principal_window, text="Details", activeforeground="black",
+                                     activebackground="white",
+                                     command=show_aadhar_details, font=('chiller', 10, 'bold'), fg="white", bg="blue")
         show_details_button.place(x=50, y=180, width=100)
         show_details_button = Button(principal_window, text="Fee Details", command=show_fee_details,
-                                     activeforeground="black",activebackground="white",font=('chiller', 10, 'bold'),fg="white",bg="blue")
+                                     activeforeground="black", activebackground="white", font=('chiller', 10, 'bold'),
+                                     fg="white", bg="blue")
         show_details_button.place(x=210, y=180, width=100)
         principal_window.mainloop()
+
     # Create the login window
     login_window = Tk()
     login_window.grab_set()
@@ -277,17 +297,19 @@ def principal_access():
     password_entry = Entry(login_window, font=(13,), show="*", bd=0)
     password_entry.place(x=10, y=150, height=25, width=240)
     Frame(login_window, width=240, height=2, bg="black").place(x=10, y=173)
-
     login_button = Button(login_window, text="Login", bg="Dark blue", fg="white",
                           font=("Microsoft YaHei UI Light", 10, "bold"), command=login)
     login_button.place(x=100, y=200, width=100)
     login_window.mainloop()
-######################################################  EXIT STUDENT ################################################
+
+
 def exit_studt():
-    rt = messagebox.askyesno("Notification", "Do you want to exit?")
-    if rt == True:
+    var = messagebox.askyesno("Notification", "Do you want to exit?")
+    if var == True:
         qwe.destroy()
-# __________________________________________________________________________________________connection of database
+
+
+#  ######################################## connection of database ####################################################
 def database_window():
     def btn_connect_server():
         global cur, conn
@@ -344,49 +366,64 @@ def database_window():
             label.config(text="C", font=('chiller', 13, 'italic bold'), bg="green")
         except mysql.connector.connect:
             label.config(text="D", font=('chiller', 13, 'italic bold'), bg="red")
+    def toggle_password():
+        if show_password.get():
+            entry_password.config(show="")
+        else:
+            entry_password.config(show="*")
     data = Toplevel()
     data.grab_set()  # close last label then first label
-    data.geometry('390x260+900+300')  # w x h
+    data.geometry('390x290+900+300')  # w x h
     data.title("DataBase Window")
     data.resizable(False, False)
     data.config(bg="white")
     # ___________________________________________________________________________________connectdb label
-    host1 = Label(data, text="Hostname : ",  bg="white", font=("times", 13, "bold"), anchor="s")
+    host1 = Label(data, text="Hostname : ", bg="white", font=("times", 14, "bold"), anchor="s")
     host1.place(x=5, y=60, height=34, width=140)
-    user = Label(data, text="Username : ",  bg="white", font=("times", 13, "bold"), anchor="s")
+    user = Label(data, text="Username : ", bg="white", font=("times", 14, "bold"), anchor="s")
     user.place(x=5, y=95, height=34, width=140)
-    password = Label(data, text="Password : ",  bg="white", font=("times", 13, "bold"), anchor="s")
-    password.place(x=5, y=125, height=34, width=140)
+    password = Label(data, text="Password : ", bg="white", font=("times", 14, "bold"), anchor="s")
+    password.place(x=5, y=132, height=34, width=140)
     password = Label(data, text="CONNECT TO DATABASE ", bg="white", font=("times", 15, "bold"), anchor="s")
     password.place(x=10, y=10, height=34, width=250)
     # ______________________________________________________________________________________entry-label of connection
     host_var = StringVar()
     user_var = StringVar()
     password_var = StringVar()
-    entry_host = Entry(data, relief=GROOVE, borderwidth=3, textvariable=host_var, font=("Time in romana", 13,), bd=0,
+    entry_host = Entry(data, relief=GROOVE, borderwidth=3, textvariable=host_var, font=("Time in romana", 14,), bd=0,
                        bg="white")
     entry_host.place(x=150, y=64, height=34, width=200)
-    Frame(data, width=200, height=2, bg="black").place(x=150, y=89)
-    entry_user = Entry(data, relief=GROOVE, borderwidth=3, bd=0, textvariable=user_var, font=("Time in romana", 13),
+    Frame(data, width=200, height=2, bg="black").place(x=150, y=90)
+    entry_user = Entry(data, relief=GROOVE, borderwidth=3, bd=0, textvariable=user_var, font=("Time in romana", 14),
                        bg="white")
     entry_user.place(x=150, y=99, height=34, width=200)
-    Frame(data, width=200, height=2, bg="black").place(x=150, y=124)
-    entry_password = Entry(data, relief=GROOVE, borderwidth=3, bd=0, textvariable=password_var,font=("Time in romana", 13),
-                           bg="white")
-    entry_password.place(x=150, y=129, height=34, width=200)
-    Frame(data, width=200, height=2, bg="black").place(x=150, y=154)
-    ####################################################     connect to database button        ##############################
+    Frame(data, width=200, height=2, bg="black").place(x=150, y=125)
+    entry_password = Entry(data, relief=GROOVE, borderwidth=3, bd=0, textvariable=password_var,
+                           font=("Time in romana", 14), bg="white", show="*")
+    entry_password.place(x=150, y=138, height=34, width=200)
+    Frame(data, width=200, height=2, bg="black").place(x=150, y=164)
+
     connect_btn = Button(data, text="   CONNECT   ", font=("Time in romana", 13, "bold"), borderwidth=3, bg="blue",
-                         width=15, activebackground='white', activeforeground='black', bd=3,fg="white", command=btn_connect_server)
-    connect_btn.place(x=125, y=190)
+                         width=15, activebackground='white', activeforeground='black', bd=3, fg="white",
+                         command=btn_connect_server)
+    connect_btn.place(x=125, y=230)
+    show_password = BooleanVar()
+    show_password.set(False)
+    show_password_checkbox = Checkbutton(data, text="Show Password", variable=show_password,
+                                         font=("Time in romana",10 , "bold"), command=toggle_password)
+    show_password_checkbox.place(x=230,y=177)
     data.mainloop()
-# ______________________________________________________________________________________  CALLING TITLE BAR
-colors = ["red", "green", "blue", "yellow", "pink", "red2"]
+
+
+# _____________________________________________________________________________________  CALLING TITLE BAR
+colors = ["red", "green", "white", "blue", "yellow", "pink", "red2", "sky blue"]
+
 
 def titlebar_color():
     fg = random.choice(colors)
     title.config(fg=fg)
     title.after(250, titlebar_color)
+
 
 def titlebar():
     global count, text
@@ -399,12 +436,16 @@ def titlebar():
         title.config(text=text)
         count += 1
     title.after(200, titlebar)
+
+
 # ___________________________________________________________________________________________________timer panel
 def timer():
     time_module = time.strftime("%r")
     date_module = time.strftime("%d-%m-%Y")
     clock.config(text=date_module + "\n" + time_module)
     clock.after(300, timer)
+
+
 # ____________________________________________________________________________________________________first panel
 qwe = Tk()
 qwe.title("Student Management System")
@@ -414,12 +455,12 @@ qwe.configure(bg="black")
 
 dataentryframe = Frame(qwe, bg="gold2")
 dataentryframe.place(x=0, y=130, height=500, width=240)
-self = Label(dataentryframe, text="CODE BY NI30SH", bg="gold2")
+self = Label(dataentryframe, text="CODE BY NI30", bg="gold2")
 self.place(x=120, y=481)
 
 im = PhotoImage(file=r"""std.png""")
-frame = Label(qwe,image = im,bg="black")
-frame.place(x=30,y=20,height=95,width=113)
+frame = Label(qwe, image=im, bg="black")
+frame.place(x=30, y=20, height=95, width=113)
 
 dataentryframe1 = Frame(qwe, bg="white")
 dataentryframe1.place(x=234, y=130, height=500, width=843)
